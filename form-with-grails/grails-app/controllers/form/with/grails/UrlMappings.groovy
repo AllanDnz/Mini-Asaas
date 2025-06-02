36e5cc2af0a71c89002dc3940c80bcdc63f78d43
Allan Barros <allancristhianjpa@gmail.com>
2025-06-02 09:09:06 -0300

    Feat: add form structure and integration with ViaCEP

---
+++ form-with-grails/grails-app/controllers/form/with/grails/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package form.with.grails
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/customer/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
