f658269f325341a7bfd3958463abaaca1d186927
Allan Barros <allancristhianjpa@gmail.com>
2025-06-02 08:56:40 -0300

    Feat: Creating domain and controller for customer

---
+++ form-with-grails/grails-app/controllers/form/with/grails/CustomerController.groovy
@@ -0,0 +1,23 @@
+package form.with.grails
+
+class CustomerController {
+
+    def index() {
+        render(view: "/customer/index")
+    }
+
+    def saveCustomer() {
+        def customer = new Customer(params)
+
+        if (!customer.validate()) {
+            customer.discard()
+            render(view: "index", model: [customer: customer])
+            return
+        }
+
+        println "Customer: ${customer.dump()}"
+
+        flash.message = "Formulário enviado com sucesso!"
+        redirect(action: "index")
+    }
+}
