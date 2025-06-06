2dc90b0eef806f5ed9ca2bd22ac7aa7a4c2c1292
Allan Barros <allancristhianjpa@gmail.com>
2025-06-06 11:31:46 -0300

    feat: implements controller and domain logic

---
+++ form/grails-app/controllers/form/customer/CustomerController.groovy
@@ -0,0 +1,24 @@
+package form.customer
+
+class CustomerController {
+
+    def index() {
+        render(view: "/customer/index")
+    }
+
+    def saveCustomer() {
+        def customer = new Customer(params)
+
+        if (!customer.validate()) {
+            customer.discard()
+            flash.message = "Por favor, corrija os erros do formulário."
+            flash.errors = customer.errors.allErrors.collect { it.defaultMessage }
+            redirect(action: "index")
+            return
+        }
+        println "Customer: ${customer.dump()}"
+
+        flash.message = "Formulário enviado com sucesso!"
+        redirect(action: "index")
+    }
+}
---
+++ form/grails-app/domain/customer/Customer.groovy
@@ -0,0 +1,24 @@
+package customer
+
+class Customer {
+    String name;
+    String cpf;
+    String phone;
+    String postalCode;
+    String street;
+    String neighborhood;
+    String city;
+    String state;
+
+    static constraints = {
+        name blank: false
+        cpf size: 11..11, matches: /\d{11}/
+        phone blank: false
+        postalCode size: 8..8, matches: /\d{8}/
+        street nullable: false
+        neighborhood nullable: false
+        city nullable: false
+        state nullable: false
+    }
+
+}
